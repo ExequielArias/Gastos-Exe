e52a5c2613f3b896c0314569bb09cedcec28a149
root <mgiraudo@ies21.edu.ar>
2023-08-25 13:09:28 -0300

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace Gastos_Exe
+{
+    partial class frmGastos
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.lblConcepto = new System.Windows.Forms.Label();
+            this.lblImporte = new System.Windows.Forms.Label();
+            this.dtpFecha = new System.Windows.Forms.DateTimePicker();
+            this.cboConcepto = new System.Windows.Forms.ComboBox();
+            this.txtImporte = new System.Windows.Forms.TextBox();
+            this.cmdCargar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.Location = new System.Drawing.Point(19, 23);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(37, 13);
+            this.lblFecha.TabIndex = 0;
+            this.lblFecha.Text = "Fecha";
+            // 
+            // lblConcepto
+            // 
+            this.lblConcepto.AutoSize = true;
+            this.lblConcepto.Location = new System.Drawing.Point(19, 68);
+            this.lblConcepto.Name = "lblConcepto";
+            this.lblConcepto.Size = new System.Drawing.Size(53, 13);
+            this.lblConcepto.TabIndex = 1;
+            this.lblConcepto.Text = "Concepto";
+            // 
+            // lblImporte
+            // 
+            this.lblImporte.AutoSize = true;
+            this.lblImporte.Location = new System.Drawing.Point(19, 107);
+            this.lblImporte.Name = "lblImporte";
+            this.lblImporte.Size = new System.Drawing.Size(42, 13);
+            this.lblImporte.TabIndex = 2;
+            this.lblImporte.Text = "Importe";
+            // 
+            // dtpFecha
+            // 
+            this.dtpFecha.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpFecha.Location = new System.Drawing.Point(91, 23);
+            this.dtpFecha.Name = "dtpFecha";
+            this.dtpFecha.Size = new System.Drawing.Size(200, 20);
+            this.dtpFecha.TabIndex = 3;
+            // 
+            // cboConcepto
+            // 
+            this.cboConcepto.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboConcepto.FormattingEnabled = true;
+            this.cboConcepto.Location = new System.Drawing.Point(91, 65);
+            this.cboConcepto.Name = "cboConcepto";
+            this.cboConcepto.Size = new System.Drawing.Size(200, 21);
+            this.cboConcepto.TabIndex = 4;
+            // 
+            // txtImporte
+            // 
+            this.txtImporte.Location = new System.Drawing.Point(91, 107);
+            this.txtImporte.Name = "txtImporte";
+            this.txtImporte.Size = new System.Drawing.Size(200, 20);
+            this.txtImporte.TabIndex = 5;
+            // 
+            // cmdCargar
+            // 
+            this.cmdCargar.Location = new System.Drawing.Point(207, 142);
+            this.cmdCargar.Name = "cmdCargar";
+            this.cmdCargar.Size = new System.Drawing.Size(84, 28);
+            this.cmdCargar.TabIndex = 6;
+            this.cmdCargar.Text = "Cargar";
+            this.cmdCargar.UseVisualStyleBackColor = true;
+            this.cmdCargar.Click += new System.EventHandler(this.cmdCargar_Click);
+            // 
+            // frmGastos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(328, 189);
+            this.Controls.Add(this.cmdCargar);
+            this.Controls.Add(this.txtImporte);
+            this.Controls.Add(this.cboConcepto);
+            this.Controls.Add(this.dtpFecha);
+            this.Controls.Add(this.lblImporte);
+            this.Controls.Add(this.lblConcepto);
+            this.Controls.Add(this.lblFecha);
+            this.Name = "frmGastos";
+            this.Text = "Gastos";
+            this.Load += new System.EventHandler(this.frmGastos_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.Label lblConcepto;
+        private System.Windows.Forms.Label lblImporte;
+        private System.Windows.Forms.DateTimePicker dtpFecha;
+        private System.Windows.Forms.ComboBox cboConcepto;
+        private System.Windows.Forms.TextBox txtImporte;
+        private System.Windows.Forms.Button cmdCargar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO; 
+
+namespace Gastos_Exe
+{
+    public partial class frmGastos : Form
+    {
+        public frmGastos()
+        {
+            InitializeComponent();
+        }
+
+        private void frmGastos_Load(object sender, EventArgs e)
+        {
+            StreamReader SR = new StreamReader("Ejemplos Conceptos.txt");
+            string linea = "";
+            while (SR.EndOfStream == false)
+            {
+                linea = SR.ReadLine();
+                cboConcepto.Items.Add(linea);
+            }
+
+
+             SR.Close();
+            SR.Dispose();
+            cboConcepto.SelectedIndex = 0; 
+        }
+
+        private void cmdCargar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Decimal importe = Convert.ToDecimal(txtImporte.Text);
+                if (importe > 0 )
+                {
+                    StreamWriter SW = new StreamWriter("Gastos.txt", true);
+                    SW.Write(dtpFecha.Text);
+                    SW.Write(", ");
+                    SW.Write(cboConcepto.Text);
+                    SW.Write(", ");
+                    SW.WriteLine(txtImporte.Text);
+                    SW.Close();
+                    SW.Dispose();
+                    cboConcepto.SelectedIndex = 0;
+                    txtImporte.Text = "";
+                }
+                else
+                {
+                    MessageBox.Show("INGRESE UN VALOR AYOR A CERO", "¡ERROR!");
+                }
+               
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("VERIFIQUE LOS DATOS INGRESADOS", "¡AVISO!"); 
+            }
+
+
+           
+        }
+    }
+}
